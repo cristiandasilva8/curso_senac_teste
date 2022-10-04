e7066ad18484978f88649a08167162faa8e3730f
Danimar Alan Albani <danimarallan@gmail.com>
2022-10-04 15:02:58 -0300

    Criei a classe CarroDoOvo no projeto

---
+++ CarroDoOvo.php
@@ -0,0 +1,23 @@
+<?php
+
+class CarroDoOvo{
+    private $modelo;
+    private $cor;
+    public function __construct(){
+        super();
+    }
+
+    public function setCor(string $cor){
+$this->cor = $cor;
+    }
+    public function setModelo(string $modelo){
+        $this->modelo = $modelo;
+    }
+    public function getCor(){
+        return $this->cor;
+    }
+    public function getModelo(){
+        return $this->modelo;
+    }
+}
+
