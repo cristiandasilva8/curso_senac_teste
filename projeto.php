c8d6100234e20628367cc08fb82ce0dfec238c57
Bruno Balen <brunobalen70@gmail.com>
2022-10-04 15:03:22 -0300

    criei a classe loja no projeto

---
+++ projeto.php
@@ -0,0 +1,31 @@
+<?php
+
+class loja{
+    private $nome;
+    private $cidade;
+  
+    public function _construct(){
+
+        super();
+    }
+
+    public function setnome(string $nome){
+        $this->nome = $nome;
+    }
+
+    public function setcidade(string $cidade){
+        $this->cidade = $cidade;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
